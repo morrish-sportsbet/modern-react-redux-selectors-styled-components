76c00090b588e5f61d4bfc5a14b1df30874732d2
Morris-Desktop <morteza.hazrati@gmail.com>
2021-02-20 08:51:41 +1100

    Basic components of the todo list app created. Next, the css will be implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
+import TodoList from "./todos/TodoList";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>Hello World</p>
+      <TodoList />
     </div>
   );
 }

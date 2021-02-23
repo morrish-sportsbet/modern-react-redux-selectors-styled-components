a3391735778e3bf050fac5ada90b689129cd3b42
Morris-Desktop <morteza.hazrati@gmail.com>
2021-02-23 15:09:13 +1100

    an async thunk created to fetch the todos data from the backend.

--- src/todos/TodoList.jsx
+++ src/todos/TodoList.jsx
@@ -1,32 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NewTodoForm from "./NewTodoForm";
 import TodoListItem from "./TodoListItem";
 import { removeTodo, markTodoAsCompleted } from "./actions";
 import { connect } from "react-redux";
-import { displayAlert } from "./thunks";
+import { displayAlert, loadTodos } from "./thunks";
 import "./TodoList.css";
 
-const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed }) => (
-  <div className="list-wrapper">
-    <NewTodoForm />
-    {todos.map((todo) => (
-      <TodoListItem
-        onCompletedPressed={onCompletedPressed}
-        onRemovePressed={onRemovePressed}
-        todo={todo}
-      />
-    ))}
-  </div>
-);
+const TodoList = ({
+  todos = [],
+  onRemovePressed,
+  onCompletedPressed,
+  isLoading,
+  startLoadingTodos,
+}) => {
+  useEffect(() => {
+    startLoadingTodos();
+  }, []);
+  const loadingMessage = <div>Loading Todos ...</div>;
+  const content = (
+    <div className="list-wrapper">
+      <NewTodoForm />
+      {todos.map((todo) => (
+        <TodoListItem
+          onCompletedPressed={onCompletedPressed}
+          onRemovePressed={onRemovePressed}
+          todo={todo}
+        />
+      ))}
+    </div>
+  );
+  return isLoading ? loadingMessage : content;
+};
 
 const mapStateToProps = (state) => ({
   todos: state.todos,
+  isLoading: state.isLoading,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   onRemovePressed: (text) => dispatch(removeTodo(text)),
   onCompletedPressed: (text) => dispatch(markTodoAsCompleted(text)),
   onDisplayAlertClicked: (text) => dispatch(displayAlert(text)),
+  startLoadingTodos: () => dispatch(loadTodos()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,3 +1,25 @@
+import {
+  loadTodosFailure,
+  loadTodosSuccess,
+  loadTodosInProgress,
+} from "./actions";
+
+export const loadTodos = () => async (dispatch, getState) => {
+  try {
+    dispatch(loadTodosInProgress());
+
+    const response = await fetch("http://localhost:8080/todos");
+    const todos = await response.json();
+
+    console.log("todos from the server is: ", todos);
+
+    dispatch(loadTodosSuccess(todos));
+  } catch (e) {
+    dispatch(loadTodosFailure());
+    dispatch(displayAlert(e));
+  }
+};
+
 export const displayAlert = (text) => () => {
-  alert(`You clicked on ${text}`);
+  alert(text);
 };
